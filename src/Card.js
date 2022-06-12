dcbc336c56113624e420dc4688a7f5b9dfbfe79f
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2022-06-12 23:33:59 +0800

    Started card component

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src="" alt="robots" />
+                <div>
+                    <h2>Jane Doe</h2>
+                    <p>jane.doe@gmail.com</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card;

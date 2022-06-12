061508fce16ce7133d60b67a950f866123492c1e
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2022-06-12 22:46:10 +0800

    Added tachyons

--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Hello.css';
+// installed npm i tachyons
 
 class Hello extends React.Component {
     render() {
         return (
-            <h1>Hello World</h1>
+            // jsx
+            <div className="f1 tc">
+                <h1>Hello World</h1>
+                <p>Welcome to React</p>
+            </div>
         )
     }
 }

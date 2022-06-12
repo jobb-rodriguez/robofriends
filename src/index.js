d7bf74c03620ad80c878026a0a80abcb03295700
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2022-06-12 23:28:28 +0800

    Removed app component files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* geeting is a prop */}
-    <Hello greeting={'Hello, React Ninja'} />
   </React.StrictMode>
 );
 

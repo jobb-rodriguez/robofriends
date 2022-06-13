90ec5c5c0204e801be11a14fa470ddd51613c9e2
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2022-06-13 10:13:20 +0800

    Added card components to index.js

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
+// <React.Fragment> removes the need for a parent element.
 
-class Card extends React.Component {
-    render() {
-        return (
+// destructured the props object
+const Card = ({ id, name, email }) => {
+    return (
+        <div className="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5">
+            <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
             <div>
-                <img src="" alt="robots" />
-                <div>
-                    <h2>Jane Doe</h2>
-                    <p>jane.doe@gmail.com</p>
-                </div>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Card;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import robots from './robots'; // use deconstructor if multiple exports
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // <React.StrictMode> only runs in development mode
   <React.StrictMode>
-    <Card />
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    </div>
   </React.StrictMode>
 );
 
